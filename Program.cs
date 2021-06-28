1753de54e138ddc2d3f4aaaca172b719caa67677
DINESHKUMAR <dk1981999@gmail.com>
2021-06-28 17:35:55 +0530

    type Conversion

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            typeConversion type = new typeConversion();
+            type.typeCast();
         }
     }
 }
---
+++ typeConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    public class typeConversion
+    {
+        public void  typeCast()
+        {
+            double d = 6.456;
+            bool b = true;
+            float f = 1.54f;
+            int i;
+
+            //cast float to integer explicit conversion
+            i = (int)d;
+            Console.WriteLine(i);
+
+            //cast various dataTypes to string
+            Console.WriteLine(b.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(f.ToString());
+        }
+    }
+}
