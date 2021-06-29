0729a995c36468509b712012dd7cf8a9c198bb64
DINESHKUMAR <dk1981999@gmail.com>
2021-06-29 11:17:55 +0530

    constants

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@ namespace helloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            typeConversion type = new typeConversion();
-            type.typeCast();
+            //Console.WriteLine("Hello World!");
+            //typeConversion type = new typeConversion();
+            //type.typeCast();
+
+            constants type = new constants();
+            type.constant();
         }
     }
 }
---
+++ constants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    //Example program for constants
+    class constants
+    {
+        public void constant()
+        {
+            const double pi = 3.14;
+            double radius;
+            Console.WriteLine("Enter Radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+
+            double areaCircle = pi * radius * radius;
+            Console.WriteLine("Radius: {0}, Area: {1}", radius, areaCircle);
+            Console.ReadLine();
+        }
+    }
+}
