45ec7ae4f22fae8aa89fa23ac59105859666eb64
DINESHKUMAR <dk1981999@gmail.com>
2021-07-01 15:47:53 +0530

    error handling

---
+++ preprocessor.cs
@@ -0,0 +1,22 @@
+﻿#define PI 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    class preprocessor
+    {
+        public void preprocessorExample()
+        {
+        #if (PI)
+            Console.WriteLine("PI is defined");
+        #else
+            Console.WriteLine("PI is not defined");
+        #endif
+            Console.ReadKey();
+        }
+    }
+}
