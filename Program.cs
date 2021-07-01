2ef16ee4e03f61915c8de708071fed4b46817170
DINESHKUMAR <dk1981999@gmail.com>
2021-07-01 10:37:12 +0530

    Static polymorphism

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace helloWorld
             int option;
             Console.WriteLine("Choose any one of the following: \n 1. HelloWorld \n 2. TypeConversion \n 3. Constants \n " +
                 "4. Operators \n 5. Loops \n 6. CallByReference&null \n 7. Arrays \n 8. Strings \n 9. Structure \n " +
-                "10. Enum\n 11. Inheritance");
+                "10. Enum\n 11. Inheritance\n 12. Polymorphism");
 
             option = Convert.ToInt32(Console.ReadLine());
             switch (option)  //example for switch and nested switch 
@@ -105,6 +105,10 @@ namespace helloWorld
                 case 11:
                     testInheritance i = new testInheritance();
                     i.inheritanceExample();
+                    break;
+                case 12:
+                    polymorphism p = new polymorphism();
+                    p.functionOverloading(); //polymorphism
                     break;
             }
         }
---
+++ polymorphism.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    //polymorphism example functionOverloading **Static Polymorphism**
+    class polymorphism
+    {
+        /*by changing number of parameters*/
+        // adding two integer values.
+        public int Add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        // adding three integer values.
+        public int Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            return sum;
+        }
+
+        /*by changing data type of parameters*/
+        public double Add(double a, double b)
+        {
+            double sum = a + b;
+            return sum;
+        }
+
+        public void functionOverloading()
+        {
+            Console.WriteLine("*****Polymorphism*****");
+            polymorphism p = new polymorphism();
+
+            int sum1 = p.Add(10, 20);
+            Console.WriteLine("Sum Of the Two Integer Variables: {0} ", sum1);
+
+            int sum2 = p.Add(20, 10, 30);
+            Console.WriteLine("Sum Of the Three Integer Variables: {0}", sum2);
+
+            double sum3 = p.Add(25.68, 78.24);
+            Console.WriteLine("Sum of Two Double Variables:{0}", sum3);
+            Console.ReadLine();
+        }
+    }
+}
